d62206758bc0c170c63b7bb4291866a3b83e37e4
Simon Hryszko <simon@hryszko.dev>
2022-09-22 18:55:47 +0200

    Change whole conception

--- Documents/Linux/Installer/Installer.sh
+++ Documents/Linux/Installer/Installer.sh
@@ -1,29 +1,17 @@
 #! /bin/bash
+set -e # Exit on error
 
-# Load system information
-# shellcheck disable=SC1091
+# install ansible 
 . /etc/os-release
-
-link=https://raw.githubusercontent.com/Shirobachi/super-duper-octo-spork/master/Documents/Linux/Installer/playbook-"$ID$VERSION_ID".yml
-if ! curl -Is "$link" | head -1 | grep -q '20'; then
-	echo "$ID $VERSION_ID is not supported!"
-	exit 1
-fi
-# Install packages
-PACHAGES="git"
-PACHAGES_UBUNTU=""
-PACHAGES_ARCH="cronie"
-if [[ "$ID" == "ubuntu" ]]; then
-	sudo apt install -y "$PACHAGES $PACHAGES_UBUNTU"
-elif [[ "$ID" == "arch" ]] || [[ "$ID" == "manjaro" ]]; then
-	sudo pacman -Syu --noconfirm "$PACHAGES $PACHAGES_ARCH"
+if [[ "$ID" == "manjaro" ]]; then
+	sudo pacman -Syu --noconfirm --needed ansible
 fi
 
-# Download bare repo
+# clone backup repo (bare)
 BACKUP_DIR="$HOME"/.backup
 # echo ".cfg" >> .gitignore &&   TODO
-git clone --bare https://github.com/Shirobachi/super-duper-octo-spork.git "$BACKUP_DIR"
-git --git-dir="$BACKUP_DIR" --work-tree="$HOME" checkout --force 
+git clone --bare https://github.com/Shirobachi/super-duper-octo-spork.git "$BACKUP_DIR" 	# cloning
+git --git-dir="$BACKUP_DIR" --work-tree="$HOME" checkout --force 							# applying
 
-# init crontab
-echo "0 0 * * * $HOME/Documents/Linux/Installer/update.sh" | crontab -
+# run ansible set up playbook
+ansible-playbook -c local -i localhost, "$HOME"/Documents/Linux/Installer/update.yml
