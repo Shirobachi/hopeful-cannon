33454eaf4265451a56f16ea907397e04a63957d7
Szymon Hryszko -Fitech-USR- <szymon.hryszko@fitech.pl>
2022-09-28 15:23:30 +0200

    update installer

--- Documents/Linux/Installer/Installer.sh
+++ Documents/Linux/Installer/Installer.sh
@@ -5,13 +5,17 @@ set -e # Exit on error
 . /etc/os-release
 if [[ "$ID" == "manjaro" ]]; then
 	sudo pacman -Syu --noconfirm --needed ansible
+else
+	echo "Unsupported OS"
+	exit 1
 fi
 
 # clone backup repo (bare)
-BACKUP_DIR="$HOME"/.backup
-# echo ".cfg" >> .gitignore &&   TODO
-git clone --bare https://github.com/Shirobachi/super-duper-octo-spork.git "$BACKUP_DIR" 	# cloning
-git --git-dir="$BACKUP_DIR" --work-tree="$HOME" checkout --force 							# applying
+BACKUP_DIR=backup
+echo ".$BACKUP_DIR" >> .gitignore
+git clone --bare https://github.com/Shirobachi/super-duper-octo-spork.git "$HOME"/."$BACKUP_DIR"
+git --git-dir="$HOME"/."$BACKUP_DIR" --work-tree="$HOME" checkout --force
+git --git-dir="$HOME"/."$BACKUP_DIR" --work-tree="$HOME" config --local status.showUntrackedFiles no
 
 # run ansible set up playbook
 ansible-playbook -c local -i localhost, "$HOME"/Documents/Linux/Installer/update.yml
