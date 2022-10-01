1f938782ef96af24cf0c8a9ab32f01fe5ba9da57
Szymon Hryszko -Fitech-USR- <szymon.hryszko@fitech.pl>
2022-10-01 14:45:32 +0200

    Update update script

--- Documents/Linux/Update.sh
+++ Documents/Linux/Update.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
+set -e # Exit on error
+set -x 
 
-BACKUP_DIR=".backup"
-BACKUP_GIT_HTTPS_REPO="https://github.com/Shirobachi/super-duper-octo-spork.git"
-GIT_PREFIX="git --git-dir=$HOME/$BACKUP_DIR --work-tree=$HOME"
-# shellcheck disable=SC1091
-. "$HOME"/Documents/Linux/.env 2>/dev/null
+# Load environment variables from .env file
+if [[ -f "$HOME"/.env ]]; then
+	# shellcheck disable=SC1091
+	. "$HOME"/.env
+fi
+
+# Set environment variables if not set
+if [[ -z "$DOI_BACKUP_REPO" ]]; then
+	DOI_BACKUP_REPO="Shirobachi/super-duper-octo-spork"
+fi
+if [[ -z "$DOI_BACKUP_DIR" ]]; then
+	DOI_BACKUP_DIR=".backup"
+fi
+if [[ -z "$DOI_BACKUP_MASTER" ]]; then
+	DOI_BACKUP_MASTER="false"
+fi
+
+BACKUP_GIT_HTTPS_REPO="https://github.com/$DOI_BACKUP_REPO.git"
+GIT_COMMAND_PREFIX="git --git-dir=$HOME/$DOI_BACKUP_DIR --work-tree=$HOME"
+
+# ======================== Functions ========================
 
 function clone_bare_repo(){
-	git clone --bare "$BACKUP_GIT_HTTPS_REPO" "$HOME"/"$BACKUP_DIR"																						# Clone (bare) repo
-	$GIT_PREFIX checkout --force																																							# Checkout files
-	$GIT_PREFIX config --local status.showUntrackedFiles no																										# Hide untracked files
+	git clone --bare "$BACKUP_GIT_HTTPS_REPO" "$HOME"/"$DOI_BACKUP_DIR"																				# Clone (bare) repo
+	$GIT_COMMAND_PREFIX checkout --force																																							# Checkout files
+	$GIT_COMMAND_PREFIX config --local status.showUntrackedFiles no																										# Hide untracked files
 }
 
 function restore_changes(){
@@ -17,7 +35,7 @@ function restore_changes(){
 	mkdir -p "$HOME"/Downloads/"$datetime"
 
 	# Move changes to Downloads
-	$GIT_PREFIX status --porcelain | while read -r line; do
+	$GIT_COMMAND_PREFIX status --porcelain | while read -r line; do
 		# Get file name and fire dir name
 		file=$(echo "$line" | awk '{print $2}')
 		fileDirectory=$(dirname "$file")
@@ -27,26 +45,27 @@ function restore_changes(){
 		mv "$HOME"/"$file" "$HOME"/Downloads/"$datetime"/"$file"
 
 		# Restore file
-		$GIT_PREFIX restore "$file"
+		$GIT_COMMAND_PREFIX restore "$file"
 
 		# Add log echo
 		echo "$file" restore to default state
+		echo "$(date +%Y-%m-%d_%H-%M-%S)" "$file" restore to default state >> "$HOME"/.logs
 	done
 }
 
 function pull_changes(){
-	$GIT_PREFIX pull --force
+	$GIT_COMMAND_PREFIX pull --force
 }
 
 function push_changes(){
 	date=$(date +%Y-%m-%d_%H-%M-%S)
 
 	# Commit all changes
-	$GIT_PREFIX commit -am "Backup update from $date"
+	$GIT_COMMAND_PREFIX commit -am "Backup update from $date"
 
 	# Pull and push
-	$GIT_PREFIX pull
-	$GIT_PREFIX push
+	$GIT_COMMAND_PREFIX pull
+	$GIT_COMMAND_PREFIX push
 }
 
 function install_and_run_ansible(){
@@ -62,22 +81,30 @@ function install_and_run_ansible(){
 
 	# Run ansible playbook in pull mode
 	ansible-pull -U "$BACKUP_GIT_HTTPS_REPO" Documents/Linux/Update.yml --purge
+}
 
+function save_env(){
+	vars=$(set | grep -e "^DOI_")
+	rm -f "$HOME"/.env
+	for var in $vars; do
+		echo "export $var" >> "$HOME"/.env
+	done
 }
 
 # ======================== Main ========================
 
 # Clone repo if not exist
-if [[ ! -d $BACKUP_DIR ]]; then
+if [[ ! -d "$HOME/$DOI_BACKUP_DIR" ]]; then
 	clone_bare_repo
 # else deal with existing repo (push or pull)
 else
-	# Check if master of backup
+	# master
 	if [[ "$DOI_BACKUP_MASTER" == "true" ]]; then
 		push_changes
+	# slave
 	else
 		# If there are changes?
-		if [[ $($GIT_PREFIX status --porcelain | wc -l) -gt 0 ]]; then
+		if [[ $($GIT_COMMAND_PREFIX status --porcelain | wc -l) -gt 0 ]]; then
 			restore_changes
 		fi
 
@@ -87,7 +114,8 @@ else
 fi
 
 install_and_run_ansible
+save_env
 
-if [[ "$#" -eq 1 ]]; then
+if [[ "$#" -eq 0 ]]; then
 	/bin/bash
 fi
