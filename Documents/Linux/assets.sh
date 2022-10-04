36c57142756282f957fc447aa1128e941768fee4
Szymon Hryszko -Fitech-USR- <szymon.hryszko@fitech.pl>
2022-10-04 18:41:57 +0200

    Fix assets

--- Documents/Linux/assets.sh
+++ Documents/Linux/assets.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 # shellcheck disable=SC1090
 
+DEFAULT_DOI_VARIABLE_FILE="$HOME/Documents/Linux/.env"
+
 # INFO: Save all variables with prefix "$2" to file "$1"
 # $1 - file to save variables to
 # $2 - variables prefix to save
 function save_variables(){
-	ENV_FILE=${1:-"$HOME/Documents/Linux/.env"}
+	ENV_FILE=${1:-"$DEFAULT_DOI_VARIABLE_FILE"}
 	VARIABLES_PREFIX=${2:-"DOI_"}
 
+	mkdir -p "$(dirname "$ENV_FILE")"
 	rm "$ENV_FILE" 2> /dev/null
 	variables=$(set | grep -e "^$VARIABLES_PREFIX")
 
@@ -19,7 +22,7 @@ function save_variables(){
 # INFO: Load all variables from file "$1"
 # $1 - file to load variables from
 function load_variables(){
-	ENV_FILE=${1:-"$HOME/Documents/Linux/.env"}
+	ENV_FILE=${1:-"$DEFAULT_DOI_VARIABLE_FILE"}
 
 	if [[ -f $ENV_FILE ]]; then
 		. "$ENV_FILE"
